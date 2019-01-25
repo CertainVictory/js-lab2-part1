20a97403be1e3bfe519c5522b30eb6d0716ddd80
Tywin <certainvictorythp@thp@gmail.com>
2019-01-25 01:39:03 -0500

    i tried

--- scripts/script.js
+++ scripts/script.js
@@ -13,23 +13,23 @@
 
 class AddressBook {
     constructor() {
-        this.contacts = [];
+    this.contacts = [];
     }
     add(info) {
         this.contacts.push(info);
         console.log(this.contacts);
-
     }
     deleteAt(index) {
         const deleted = this.contacts.splice(index, 1);
         console.log(deleted);
     }
-    deleteByName(name) {
-        book.deleteAt(name)
-        console.log();
+    deleteByName(mmm) {
+        const whoWasDeleted = this.contacts.splice(this.contacts.indexOf(whoToDelete));
+        console.log(whoWasDeleted);
     }
     print() {
         for (let i = 0; i < this.contacts.length; i++) {
+
             console.log(this.contacts[i]);
         }
     }
@@ -52,24 +52,25 @@ const book = new AddressBook();
 book.add(new Contact("Snoopy", "snoopy@cool.com", "3323", "Friend"));
 book.add(new Contact("Woodstock", "woodstock@cool.com", "123", "Aunt"));
 book.add(new Contact("Rob", "rob@cool.com", "122223", "Uncle"));
+book.add(new Contact("Jo", "jo@cool.com", "345", "Idk"));
 
 
 
 while (true) {
-    var choice = prompt("add, print, delete, quit?")
-    if (choice === "add"){
+    var choice = prompt("add, print, delete, delete by name, quit?")
+    if (choice === "add") {
         const name = prompt("Enter name");
         const email = prompt("Enter email");
         const phone = prompt("Enter phone");
         const relation = prompt("Enter relation");
         book.add(new Contact(name, email, phone, relation));
-    }else if (choice === "print") {
+    } else if (choice === "print") {
         book.print();
-    }else if (choice === "delete") {
+    } else if (choice === "delete") {
         var indexToDelete = prompt("Index to delete?")
         book.deleteAt(indexToDelete);
         console.log(indexToDelete)
-    }else if(choice === "delete by name") {
+    } else if (choice === "delete by name") {
         var whoToDelete = prompt("What name to delete?")
         book.deleteByName(whoToDelete);
         console.log(whoToDelete);
